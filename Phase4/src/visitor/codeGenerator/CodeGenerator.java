17a5e9b57ba593159b665bb39437c0a0bfbb8a94
Javad <javad.besharati78@gmail.com>
2023-07-09 12:39:58 +0330

    Just junk comments and white spaces cleared

--- Phase4/src/visitor/codeGenerator/CodeGenerator.java
+++ Phase4/src/visitor/codeGenerator/CodeGenerator.java
@@ -36,7 +36,8 @@ public class CodeGenerator extends Visitor<Void> {
     private Void run_jasmin_code() {
         try (PrintWriter out = new PrintWriter("result.j")) {
             out.println(commands);
-        } catch (FileNotFoundException ignored) {}
+        } catch (FileNotFoundException ignored) {
+        }
 
         try {
             Runtime.getRuntime().exec("java -jar src\\jasmin.jar src\\*.j");
@@ -55,11 +56,11 @@ public class CodeGenerator extends Visitor<Void> {
         return localVars.indexOf(str);
     }
 
-    private boolean is_valid_istore_load_x (String str) {
+    private boolean is_valid_istore_load_x(String str) {
         return (str.equals("0") || str.equals("1") || str.equals("2") || str.equals("3"));
     }
 
-    private boolean is_valid_iconst_x (String str) {
+    private boolean is_valid_iconst_x(String str) {
         return (str.equals("0") || str.equals("1") || str.equals("2") || (str.equals("3"))
                 || str.equals("4") || (str.equals("5")));
     }
@@ -80,14 +81,10 @@ public class CodeGenerator extends Visitor<Void> {
     }
 
     @Override
-    public Void visit(MainDeclaration mainDeclaration)
-    {
+    public Void visit(MainDeclaration mainDeclaration) {
         add_commands(".method public static main([Ljava/lang/String;)V");
         add_commands(".limit stack " + 128);
         add_commands(".limit locals " + 128);
-
-//        MainItem main_item = new MainItem(mainDeclaration);
-//        SymbolTable.push(main_item.getMainItemSymbolTable());
 
         for (var stmt : mainDeclaration.getMainStatements()) {
             stmt.accept(this);
@@ -105,58 +102,51 @@ public class CodeGenerator extends Visitor<Void> {
         add_commands(".limit stack " + 128);
         add_commands(".limit locals " + 128);
 
-//        try {
-//            FunctionItem functionItem = (FunctionItem) SymbolTable.root
-//                    .get(FunctionItem.STARTKEY + functionDeclaration.getName().getName());
-//            SymbolTable.push((functionItem.getFunctionSymbolTable()));
-//        } catch (ItemNotFoundException ignored) {}
-
         for (var stmt : functionDeclaration.getStatements()) {
             stmt.accept(this);
         }
 
         add_commands(".end method");
-//        SymbolTable.pop();
+
         return null;
     }
 
     @Override
-    public Void visit(FunctionCall functionCall)
-    {
+    public Void visit(FunctionCall functionCall) {
         String functionName = functionCall.getUFuncName().getName();
         add_commands("invokestatic Main/" + functionName + "()I");
         return null;
     }
 
     @Override
-    public Void visit(ReturnStmt returnStmt)
-    {
+    public Void visit(ReturnStmt returnStmt) {
         returnStmt.getExpression().accept(this);
         add_commands("ireturn");
         return null;
     }
 
     @Override
-    public Void visit(AssignStmt assignStmt)
-    {
+    public Void visit(AssignStmt assignStmt) {
         boolean accessible = false;
-        if (assignStmt.getLValue() instanceof ArrayAccess)
-        {
-            String idx = find_local_var_index(assignStmt.getLValue().toString().replace("ArrayAccess ", ""))
-                    .toString();
+
+        if (assignStmt.getLValue() instanceof ArrayAccess) {
+            String idx = find_local_var_index(assignStmt.getLValue().toString().replace("ArrayAccess ", "")).toString();
+
             if (is_valid_istore_load_x(idx))
                 add_commands("aload_" + idx);
             else
                 add_commands("aload " + idx);
             accessible = true;
         }
+
         assignStmt.getRValue().accept(this);
         String local_var_index = find_local_var_index(assignStmt.getLValue().toString()).toString();
-        if (accessible)
-        {
+
+        if (accessible) {
             add_commands("iastore");
             return null;
         }
+
         if (is_valid_istore_load_x(local_var_index))
             add_commands("istore_" + local_var_index);
         else
@@ -165,11 +155,11 @@ public class CodeGenerator extends Visitor<Void> {
     }
 
     @Override
-    public Void visit(VarDecStmt varDecStmt)
-    {
+    public Void visit(VarDecStmt varDecStmt) {
         varDecStmt.getInitialExpression().accept(this);
         localVars.add(varDecStmt.getIdentifier().getName());
         String local_var_index = find_local_var_index(varDecStmt.getIdentifier().toString()).toString();
+
         if (is_valid_istore_load_x(local_var_index))
             add_commands("istore_" + local_var_index);
         else
@@ -178,8 +168,7 @@ public class CodeGenerator extends Visitor<Void> {
     }
 
     @Override
-    public Void visit(UnaryExpression unaryExpression)
-    {
+    public Void visit(UnaryExpression unaryExpression) {
         unaryExpression.getOperand().accept(this);
 
         if (unaryExpression.getUnaryOperator().equals(UnaryOperator.minus)) {
@@ -190,31 +179,26 @@ public class CodeGenerator extends Visitor<Void> {
     }
 
     @Override
-    public Void visit(BinaryExpression binaryExpression)
-    {
+    public Void visit(BinaryExpression binaryExpression) {
         BinaryOperator operator = binaryExpression.getBinaryOperator();
-        if (operator == BinaryOperator.add)
-        {
+
+        if (operator == BinaryOperator.add) {
             binaryExpression.getLeft().accept(this);
             binaryExpression.getRight().accept(this);
             add_commands("iadd");
-        } else if (operator == BinaryOperator.sub)
-        {
+        } else if (operator == BinaryOperator.sub) {
             binaryExpression.getLeft().accept(this);
             binaryExpression.getRight().accept(this);
             add_commands("isub");
-        } else if (operator == BinaryOperator.mult)
-        {
+        } else if (operator == BinaryOperator.mult) {
             binaryExpression.getLeft().accept(this);
             binaryExpression.getRight().accept(this);
             add_commands("imul");
-        } else if (operator == BinaryOperator.div)
-        {
+        } else if (operator == BinaryOperator.div) {
             binaryExpression.getLeft().accept(this);
             binaryExpression.getRight().accept(this);
             add_commands("idiv");
-        } else if (operator == BinaryOperator.mod)
-        {
+        } else if (operator == BinaryOperator.mod) {
             binaryExpression.getLeft().accept(this);
             binaryExpression.getRight().accept(this);
             add_commands("irem");
@@ -227,6 +211,7 @@ public class CodeGenerator extends Visitor<Void> {
     public Void visit(Identifier identifier) {
 
         String local_var_index = find_local_var_index(identifier.getName()).toString();
+
         if (is_valid_istore_load_x(local_var_index))
             add_commands("iload_" + local_var_index);
         else
@@ -236,14 +221,15 @@ public class CodeGenerator extends Visitor<Void> {
     }
 
     @Override
-    public Void visit(IntValue value)
-    {
+    public Void visit(IntValue value) {
         String val = value.toString().replace("IntValue ", "");
-        if (is_valid_iconst_x(val)){
+
+        if (is_valid_iconst_x(val)) {
             add_commands("iconst_" + val);
         } else {
             add_commands("bipush " + val);
         }
+
         return null;
     }
 
